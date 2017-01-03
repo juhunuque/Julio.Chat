ac570ef31722fc9e5709efa9e31a8d47eab30a37
Julio N <juhunuque@gmail.com>
2017-01-03 07:44:13 -0600

    commit deploy

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ io.on("connection", (socket)=>{
 	});
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;;
 server.listen(port, ()=>{
 	console.log(`Server started on port ${port}`);
 });
